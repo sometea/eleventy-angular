1922c92467bc7221af160590c0c89a14027f9f66
David Dasenbrook <david.dasenbrook@tngtech.com>
2020-08-29 11:14:36 +0200

    move content into its own directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("posts", function (collectionApi) {
-      return collectionApi.getFilteredByGlob("content/posts/*.md");
+      return collectionApi.getFilteredByGlob("templates/content/posts/*.md");
   });
   return {
     dir: {
-      input: "content",
+      input: "templates",
       includes: "../_includes",
     },
   };
