bf48bbea0a874b98187ecdf16447ebb1982c00aa
sometea <sometea@users.noreply.github.com>
2020-10-24 15:27:03 +0200

    Update post-detail.component.ts
    Make it possible to style inline html from blog posts.

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { PostDetail } from '../post.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.css']
+  styleUrls: ['./post-detail.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PostDetailComponent {
   @Input() post: PostDetail;
